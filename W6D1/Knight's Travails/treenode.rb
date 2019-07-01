d1b75375d83cf2493dc6be89dca227298f22267a
StanBond <stan.bond@ymail.com>
2019-07-01 17:26:21 -0600

    "KNIGHTS_TRAVAILS part1, part2 (incom)"

--- W6D1/Knight's Travails/treenode.rb
+++ W6D1/Knight's Travails/treenode.rb
@@ -1,24 +1,51 @@
 class TreeNode
-    attr_accessor :parent, :children, :value, :path, :counter
+    attr_accessor :parent, :children, :value
 
-  def initialize(parent, value)
-    @parent = parent
-    @children = []
-    @value = value
-    @path = []
-    @counter = 0
-  end
+    def initialize(value, parent=nil)
+      @parent = parent
+      @children = []
+      @value = value
+    end
 
-  def print_tree
-    
-  end
-
+  
   def add_child(child_node)
-
+    child_node.parent && child_node.parent.remove_child(child_node)
+    @children << child_node
+    child_node.parent = self
+    child_node
   end
 
-  def delete_child(child_node)
+  def remove_child(child_node)
+    @children.delete(child_node)
+    child_node.parent = nil
+    nil
+  end
 
+  def dfs(value)
+    return self if @value == value
+    @children.each do |child|
+      child_val = child.dfs(value)
+      return child_val if child_val
+    end
+    nil
+  end
+
+  def bfs(value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == value
+      queue += current_node.children
+    end
+    nil
+  end
+
+  def path
+    if self.parent.nil?
+      return [self.value]
+    else
+      self.parent.path << self.value
+    end
   end
 
 end
