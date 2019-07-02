71a6b19d76b414aaef7944e10afb80404cc9f00a
StanBond <stan.bond@ymail.com>
2019-07-02 17:33:02 -0600

    "Worked on SuperPC, needs more work"

--- W6D2/TTT_AI/lib/tic_tac_toe_node.rb
+++ W6D2/TTT_AI/lib/tic_tac_toe_node.rb
@@ -6,38 +6,30 @@ class TicTacToeNode
 
   def initialize(board, mark, prev_move_pos = nil)
     @board = board
-    @next_mover_mark = mark #(mark == :o ? :x : :o)
-    @prev_move_pos = prev_move_pos # check later
-    # @op_mark = other_player(@mark)
-    #@children = children
-    # @parent = parent
+    @next_mover_mark = mark #:x,:o
+    @prev_move_pos = prev_move_pos 
   end
 
-  def losing_node?(evaluator)
-
+  def losing_node?(evaluator) #evaluator is human_player-:x or computer_player-:o
+    !winning_node?(evaluator)  
   end
 
-  def winning_node?(evaluator)
-    if board.winner == evaluator
-      return true
-    elsif @children.empty?
-      return false
-    else
-      if evaluator == @current_player
-        #player's turn, one child node wins
-        @children.any? { |child| child.winning_node?(evaluator) }
-      elsif evaluator == @next_player
-        #opponent's turn, all child nodes win
-        @children.all? { |child| child.winning_node?(evaluator) }
-      else
-        #tie or lose
-        return false
-      end
+  def winning_node?(evaluator) # board_node.winning_node?(:x)
+    if @board.over?
+      return @board.winner == evaluator
+    else  
+      if next_mover_mark == evaluator
+        self.children.any? do |child| 
+          child.winning_node?(evaluator)
+        end
+      else  
+        self.children.all? do |child|
+          child.winning_node?(evaluator)
+        end
+      end      
     end
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
   def children
     # debugger
     positions = []
@@ -57,31 +49,3 @@ class TicTacToeNode
   end
   
 end
-
-# new_node = TicTacToeNode.new(Board.new, :x)
-# new_node.children
-
-    # possible_moves = []
-    #   updated_board = @board
-
-    #   updated_board.rows.each.with_index do |row, idx|
-    #     row.each_index do |pos|
-    #       if updated_board.rows[idx][pos].nil?
-    #         #puts "#{idx}, #{pos}"
-
-    #         updated_board.rows[idx][pos] = @mark
-    #         child = TicTacToeNode.new(updated_board, self, [idx, pos])
-
-    #         self.add_child(child)
-    #         all_possible_moves << child
-    #         updated_board.rows[idx][pos] = nil
-    #       end
-    #     end
-    #   end
-
-    #   all_possible_moves
-    # end
-
-    # def add_child(child)
-    #   self.children << child
-    #   child.prev_move_pos = self
