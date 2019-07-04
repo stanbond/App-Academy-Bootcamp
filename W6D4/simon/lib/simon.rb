66fb89e097a5cba9322ed6ad39debcb1823b4dfb
StanBond <stan.bond@ymail.com>
2019-07-03 20:29:51 -0600

    "filled in simon and fixed spec color issue"

--- W6D4/simon/lib/simon.rb
+++ W6D4/simon/lib/simon.rb
@@ -4,38 +4,56 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until game_over
+      take_turn
+    end
 
+    if game_over
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !game_over
+      round_success_message
+      self.sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
-
+    puts "repeat after me"
+    take_turn
+    guess = gets.chomp
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "great job"
   end
 
   def game_over_message
-
+    puts "GAME OVER"
   end
 
   def reset_game
-
+    self.sequence_length = 1
+    self.game_over = false
+    self.seq = []
   end
 end
