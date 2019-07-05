4423c1075be1c153b0b689c054ba2b550ca9447c
StanBond <stan.bond@ymail.com>
2019-07-05 15:52:57 -0600

    "added more specs to pass"

--- W6D5/tdd/spec/tdd_project_spec.rb
+++ W6D5/tdd/spec/tdd_project_spec.rb
@@ -7,12 +7,39 @@ describe Array do
         it "removes the duplicates of the array" do
             expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
         end
+
+        it "works without duplicates" do
+            expect([1, 2, 3].my_uniq).to eq([1, 2, 3])
+        end
+
+        it "works with duplicates" do
+            expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
+        end
+
+        it "does not modify original array" do
+        array = [1, 2, 1, 3, 3]
+        array.my_uniq
+            expect(array).to eq([1, 2, 1, 3, 3])
+        end
     end
     
     describe "#two_sum" do
         it "finds index of sums of 0" do
             expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
         end
+
+        it "returns empty array if array is empty" do
+            expect([].two_sum).to eq([])
+        end
+
+        it "empty if no matches" do
+            expect([1,2,3].two_sum).to eq([])
+        end
+
+        it "doesn't duplicate matches" do
+            expect([1,-1].two_sum).to eq([[0,1]])
+        end
+
     end
 
     describe "#my_transpose" do
