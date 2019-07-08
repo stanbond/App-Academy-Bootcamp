826b5195582e8591e6e42622143aa9eecfa19d3d
StanBond <stan.bond@ymail.com>
2019-07-08 14:17:03 -0600

    "finalized mancala"

--- W6D3/mancala/lib/board.rb
+++ W6D3/mancala/lib/board.rb
@@ -23,10 +23,34 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos] 
+    @cups[start_pos] = []
+    
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end 
+    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
