d28d3d53b346c9e3257803365efc08383e8efbe5
StanBond <stan.bond@ymail.com>
2019-07-14 18:28:58 -0600

    "Worked on playwright class"

--- W8D1_SQL/plays_playwrights_orm/plays.rb
+++ W8D1_SQL/plays_playwrights_orm/plays.rb
@@ -42,7 +42,7 @@ class Play
     play = PlayDBConnection.instance.execute(<<-SQL, title)
       SELECT
         *
-      FROM 
+      FROM
         plays
       WHERE
         title = ?
@@ -53,11 +53,11 @@ class Play
     play = PlayDBConnection.instance.execute(<<-SQL, name)
       SELECT
         *
-      FROM 
+      FROM
         plays
       JOIN
         playwrights
-      ON 
+      ON
         plays.playwrights_id = play.id
       WHERE
         playwright.name = ?
@@ -76,3 +76,58 @@ class Play
     SQL
   end
 end
+
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    author = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT
+      *
+    FROM
+      playwrights
+    WHERE
+      name = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options["id"]
+    @name = options["name"]
+    @birth_year = options["birth_year"]
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
+
+# Playwright::all --
+# Playwright::find_by_name(name) --
+# Playwright#new (this is the initialize method) --
+# Playwright#create -- 
+# Playwright#update --
+# Playwright#get_plays (returns all plays written by playwright)
