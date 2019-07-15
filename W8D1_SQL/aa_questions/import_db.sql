c1cefccde2ae5b5ec0f342bf59250d1d3ef4a9f1
StanBond <stan.bond@ymail.com>
2019-07-15 15:14:53 -0600

    "fixed bug in import_db.sql file, filled in questions.rb"

--- W8D1_SQL/aa_questions/import_db.sql
+++ W8D1_SQL/aa_questions/import_db.sql
@@ -21,16 +21,16 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE question_follows (
-    id SERIAL PRIMARY KEY,
-    questions_id INTEGER NOT NULL,
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
 
-    FOREIGN KEY (questions_id) REFERENCES questions(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE replies (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     body TEXT NOT NULL,
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
@@ -42,11 +42,11 @@ CREATE TABLE replies (
 );
 
 CREATE TABLE question_likes (
-    id SERIAL PRIMARY KEY,
-    questions_id INTEGER NOT NULL,
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
 
-    FOREIGN KEY (questions_id) REFERENCES questions(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 
 );
@@ -67,7 +67,7 @@ VALUES
 
 
 INSERT INTO
-    question_follows (questions_id, user_id)
+    question_follows (question_id, user_id)
 VALUES
     (1,1),
     (2,2),
@@ -81,7 +81,7 @@ VALUES
     ('It''s a base case ever', (SELECT id FROM questions WHERE body = 'what is a base case?'), (SELECT id FROM users WHERE fname = 'Carlos'), NULL);
 
 INSERT INTO
-    question_likes (questions_id, user_id)
+    question_likes (question_id, user_id)
 VALUES
     (1,2),
     (2,1),
--- W8D1_SQL/aa_questions/questions.rb
+++ W8D1_SQL/aa_questions/questions.rb
@@ -19,6 +19,50 @@ class User
     data.map {|datum| User.new(datum)}
   end
 
+  def sql_find_by_id(id) #SQL VERSION
+    results = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        user
+      WHERE
+        id = ?
+    SQL
+
+    return "Not found" if results.empty?
+    self.new(results.first)
+  end
+
+  def find_by_id(id) #RUBY VERSION
+    find = self.all
+    find.each do |user|
+      return user if user.id == id
+    end
+    return false
+  end
+
+  def find_by_name(fname, lname) #RUBY VERSION
+    find = self.all
+    find.each do |user|
+      return user if user.fname == fname && user.lname == lname
+    end
+    return false
+  end
+
+  def self.sql_find_by_name(fname, lname) #SQL VERSION
+    results = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        user
+      WHERE
+        fname = ? AND lname = ?
+    SQL
+
+    return "NON_EXISTANT" if results.empty?
+    self.new(results.first)
+  end
+
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
@@ -26,3 +70,117 @@ class User
   end
 
 end
+
+class Question #< User
+    attr_accessor :id, :title, :body, :author_id
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map {|datum| Question.new(datum)}
+  end
+
+  def find_by_id(id) #RUBY VERSION
+    find = self.all
+    find.each do |question|
+      return question if question.id == id
+    end
+    return false
+  end
+
+end
+
+class Question_follow
+    attr_accessor :id, :question_id, :user_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+    data.map {|datum| Question_follow.new(datum)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+
+  def find_by_id(id) #RUBY VERSION
+    find = self.all
+    find.each do |question|
+      return question if question.id == id
+    end
+    return false
+  end
+
+end
+
+  
+class Reply
+  attr_accessor :id, :question_id, :user_id, :body, :parent_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map {|datum| Reply.new(datum)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @body = options['body']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+    @parent_id = options['parent_id']
+  end
+
+  def find_by_id(id) #RUBY VERSION
+    find = self.all
+    find.each do |question|
+      return question if question.id == id
+    end
+    return false
+  end
+end
+
+class Question_like
+    attr_accessor :id, :question_id, :user_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
+    data.map {|datum| Question_like.new(datum)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+
+  def find_by_id(id) #RUBY VERSION
+    find = self.all
+    find.each do |question|
+      return question if question.id == id
+    end
+    return false
+  end
+end
+
+
+
+# Question::find_by_author_id(author_id)
+# Reply::find_by_user_id(user_id)
+# Reply::find_by_question_id(question_id)
+# All replies to the question at any depth.
+# User::find_by_name(fname, lname)
+# User#authored_questions (use Question::find_by_author_id)
+# User#authored_replies (use Reply::find_by_user_id)
+# Question#author
+# Question#replies (use Reply::find_by_question_id)
+# Reply#author
+# Reply#question
+# Reply#parent_reply
+# Reply#child_replies
