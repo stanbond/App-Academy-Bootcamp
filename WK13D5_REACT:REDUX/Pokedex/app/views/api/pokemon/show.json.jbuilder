012539b7c1f509a04b9316eed52634ea404899a9
StanBond <stan.bond@ymail.com>
2019-08-26 19:57:51 -0600

    end of day upload

--- WK13D5_REACT:REDUX/Pokedex/app/views/api/pokemon/show.json.jbuilder
+++
@@ -1,5 +0,0 @@
-
-json.extract! @pokemon, :id, :name, :attack, :defense, :poke_type, :moves, :items, :image_url
-json.image_url asset_path(@pokemon.image_url)
-
-
--- WK13D5_REACT:REDUX/Pokedex/frontend/reducers/entities_reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import pokemonReducer from './pokemon_reducer';
-
-export const entitiesReducer = combineReducers({
-    pokemon: pokemonReducer,
-})
