dde6e03392f611c7470d3e371c983d65385e4f30
StanBond <stan.bond@ymail.com>
2019-08-29 21:27:01 -0600

    end of day commit

--- BenchBnB/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'static_pages#root'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: {format: :json} do    
-    resources :users, only: [:create, :show]
-    resource :session, only: [:create, :destroy]
-  end
-end
