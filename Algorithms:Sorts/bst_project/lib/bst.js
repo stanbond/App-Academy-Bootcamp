8bdeb9c472c216b8bce680203ae5706fefc5aebd
StanBond <stan.bond@ymail.com>
2019-10-31 13:48:30 -0600

    bst

--- Algorithms:Sorts/bst_project/lib/bst.js
+++ Algorithms:Sorts/bst_project/lib/bst.js
@@ -5,10 +5,57 @@ class TreeNode {
         this.right = null;
     }
 }
-
-
 class BST {
-   
+   constructor() {
+       this.root = null;
+   }
+
+   insert(val, root=this.root) {
+       if(!this.root) {
+           this.root = new TreeNode(val);
+           return;
+       }
+
+       if (val < root.val) {
+           if(!root.left) {
+               root.left = new TreeNode(val);
+           } else {
+               this.insert(val, root.left)
+           }
+       } else {
+           if (!root.right) {
+               root.right = new TreeNode(val);
+           } else {
+               this.insert(val, root.right)
+           }
+       }
+   }
+
+   searchRecur(val, root = this.root) {
+       if (!root) return false
+        if (val < root.val){
+            return this.searchRecur(val, root.left)
+        } else if (val > root.val) {
+            return this.searchRecur(val, root.right)
+        } else {
+            return true
+        }
+   }
+
+   searchIter(val){
+    let currentNode = this.root
+
+        while (currentNode) {
+            if (val > currentNode.val) {
+                currentNode = currentNode.right
+            } else if (val < currentNode.val) {
+                currentNode = currentNode.left
+            } else {
+              return true
+            }
+        }
+        return false
+   }
 }
 
 module.exports = {
