35a152ac89c6ec4ad5c286a09413f7375876707b
StanBond <stan.bond@ymail.com>
2019-10-31 13:59:12 -0600

    prob 3

--- Algorithms:Sorts/bst_project/lib/leet_code_110.js
+++ Algorithms:Sorts/bst_project/lib/leet_code_110.js
@@ -1,3 +1,17 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+function maxHeight(root) {
+  if (!root) return -1;
+  return Math.max(maxHeight(root.left), maxHeight(root.right)) + 1;
+}
+
+var isBalanced = function (root) {
+  if (!root) return true;
+
+  let leftHeight = maxHeight(root.left)
+  let rightHeight = maxHeight(root.right)
+
+  let diff = Math.abs(leftHeight - rightHeight) <= 1;
+  return diff && isBalanced(root.left) && isBalanced(root.right);
+};
