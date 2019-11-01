5cde405373c879be299a29038d0f028ce3b14c07
StanBond <stan.bond@ymail.com>
2019-11-01 14:46:37 -0600

    leet code

--- Algorithms:Sorts/graph_project/lib/leet_code_207.js
+++ Algorithms:Sorts/graph_project/lib/leet_code_207.js
@@ -1,6 +1,40 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
+function buildGraph(edges) {
+  let graph = {}
+  edges.forEach((ele) => {
+    let [ destination, src ] = ele.map(String);
+      if (destination in graph) {
+        graph[destination].push(src);
+      } else {
+        graph[destination] = [ src ];
+      }
 
+      if(!(src in graph)) {
+        graph[src] = [];
+      }
+  });
+  return graph;
+  
+}
+
+function canFinish(numCourses, prerequisites) {
+  let graph = buildGraph(prerequisites);
+  let totalCourses = Object.keys(graph).length;
+  let visited = new Set();
+
+    let eligibleNodeExists = true;
+    while (eligibleNodeExists) {
+      eligibleNodeExists = false;
+    }
+    
+  for(let destination in graph){
+    let validDestination = graph[destination].every((ele) => visited.has(ele));
+      if(!visited.has(destination) && validDestination){
+        eligibleNodeExists = true;
+        visited.add(destination)
+      }
+  }
+  return visited.size === totalCourses
 }
