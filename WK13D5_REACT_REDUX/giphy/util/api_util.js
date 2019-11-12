fcac6d9f47a2c47c361bd02df5ccb9dbbdc24002
StanBond <stan.bond@ymail.com>
2019-11-11 22:14:15 -0700

    clean

--- WK13D5_REACT_REDUX/giphy/util/api_util.js
+++
@@ -1,7 +0,0 @@
-export const fetchSearchGiphys = (fetched) => {
-  $.ajax({
-    method: "GET",
-    url: `http://api.giphy.com/v1/gifs/search?q=${fetched}&api_key=dc6zaTOxFJmzC&limit=2 where the ${fetched}`
-  });
-};
-
